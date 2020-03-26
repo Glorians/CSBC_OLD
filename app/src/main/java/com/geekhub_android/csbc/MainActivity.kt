83b413017a2858a1a77ad8481d694568d4720228
Roman <culmgenius231289@gmail.com>
2020-03-26 17:20:49 +0200

    Exit About TeacherActivity,creat StudentActivity

--- app/src/main/java/com/geekhub_android/csbc/MainActivity.kt
+++ app/src/main/java/com/geekhub_android/csbc/MainActivity.kt
@@ -1,70 +1,38 @@
 package com.geekhub_android.csbc
 
 
-import ViewPager2.ExampleStateAdapter
-import android.annotation.TargetApi
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import android.content.ContentValues
-import android.content.Context
-import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import androidx.viewpager2.widget.ViewPager2
-import com.geekhub_android.csbc.DBHelper.ReaderDBHelper
-import com.geekhub_android.csbc.DBHelper.SubjectDB
-import com.geekhub_android.csbc.Model.Subject
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.getValue
 
-import java.util.*
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 
 class MainActivity() : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initViewPager2WithFragments()
+
+        button1.setOnClickListener{
+            val intent = Intent (this,TeacherActivity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener{
+            val intent = Intent (this,StudentActivity::class.java)
+            startActivity(intent)
+        }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main,menu)
+        return true
 
-    @TargetApi(Build.VERSION_CODES.O)
-    private fun initViewPager2WithFragments() {
-        var viewPager: ViewPager2 = findViewById(R.id.viewpager)
-        var adapter = ExampleStateAdapter(
-            supportFragmentManager,
-            lifecycle,
-            this
-        )
-        viewPager.adapter = adapter
-
-        var tabLayout:TabLayout = findViewById(R.id.tablayout)
-        var names = mutableListOf<String>("Today")
-
-        val instance = Calendar.getInstance()
-
-        fun splitData (data: String): String {
-            var result = ""
-            val list = data.split(" ")
-            for (i in 0..2) {
-                result += list[i] + " "
-            }
-            return result
-        }
-        for (i in 1..6) {
-            instance.add(Calendar.DAY_OF_WEEK, 1)
-            names.add(splitData( instance.time.toString()))
-        }
-
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = names[position]
-        }.attach()
     }
 }
 
---
+++ app/src/main/java/com/geekhub_android/csbc/StudentActivity.kt
@@ -0,0 +1,55 @@
+package com.geekhub_android.csbc
+
+import ViewPager2.ExampleStateAdapter
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import java.util.*
+
+class StudentActivity : AppCompatActivity() {
+    private val cont = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student)
+        initViewPager2WithFragments(cont)
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private fun initViewPager2WithFragments(context: Context) {
+        var viewPager: ViewPager2 = findViewById(R.id.viewpager)
+        var adapter = ExampleStateAdapter(
+            supportFragmentManager,
+            lifecycle,context
+        )
+        viewPager.adapter = adapter
+
+        var tabLayout: TabLayout = findViewById(R.id.tablayout)
+        var names = mutableListOf<String>("Today")
+
+        val instance = Calendar.getInstance()
+
+        fun splitData (data: String): String {
+            var result = ""
+            val list = data.split(" ")
+            for (i in 0..2) {
+                result += list[i] + " "
+            }
+            return result
+        }
+        for (i in 1..6) {
+            instance.add(Calendar.DAY_OF_WEEK, 1)
+            names.add(splitData( instance.time.toString()))
+        }
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = names[position]
+        }.attach()
+    }
+}
