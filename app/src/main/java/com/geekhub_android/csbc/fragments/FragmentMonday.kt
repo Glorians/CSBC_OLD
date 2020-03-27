a0bb427d0207f4808717978431e2b744e75ad511
Roman <culmgenius231289@gmail.com>
2020-03-27 14:50:52 +0200

    add recyclerView to Fragment

--- app/src/main/java/com/geekhub_android/csbc/fragments/FragmentMonday.kt
+++ app/src/main/java/com/geekhub_android/csbc/fragments/FragmentMonday.kt
@@ -1,6 +1,5 @@
 package com.geekhub_android.csbc.fragments
 
-import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
